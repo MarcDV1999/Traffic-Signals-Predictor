955faf4ae2f56fbc8ee549d197a62a06814a6d08
Marc Domènech <marcdomenechvila@gmail.com>
2021-01-15 03:43:57 +0100

    Update main.m

--- Code/main.m
+++ Code/main.m
@@ -3,14 +3,13 @@ addpath('OtherFunctions');
 addpath('Segmentation');
 addpath('Features');
 
-%% Load Image
+%% Carreguem Imatges d'exemple
 fname = '../Images/Train1/1/00001_00006_00024.png';
 fname2 = '../Images/Train1/1/00001_00008_00024.png';
 fname3 = '../Images/Train1/12/00012_00000_00000.png';
 im = imread(fname);
 im2 = imread(fname2);
 im3 = imread(fname3);
-% Conseguim quin tipus de senyal es
 
 
 
